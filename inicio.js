722f98df3b3e185c2411fa9825a5b782ee70bd63
EdgardQO <145729503+EdgardQO@users.noreply.github.com>
2024-05-23 22:39:13 -0500

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -2,5 +2,5 @@ document.getElementById("togglePassword").addEventListener("click", function() {
     const passwordField = document.getElementById("contrasena");
     const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
     passwordField.setAttribute("type", type);
-    this.textContent = type === "password" ? "✅" : "❌";
+    this.textContent = type === "password" ? "🔒" : "🔓";
 });
